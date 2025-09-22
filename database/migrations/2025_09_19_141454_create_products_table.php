286fd01dc418bc66ea03c58c63f5e2a3efff35e3
kottor <gardien243@gmail.com>
2025-09-22 09:23:50 +0200

    j'ia oubier de sav

--- database/migrations/2025_09_19_141454_create_products_table.php
+++ database/migrations/2025_09_19_141454_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('color');
+            $table->text('description');
+            $table->integer('price');
+            $table->integer('stock');
+            
             $table->timestamps();
         });
     }
