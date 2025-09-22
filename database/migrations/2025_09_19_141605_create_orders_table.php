fc01e8f681a4b52e7279a97da3e4fcc071c05f0e
kottor <gardien243@gmail.com>
2025-09-22 14:35:52 +0200

    oooo

--- database/migrations/2025_09_19_141605_create_orders_table.php
+++ database/migrations/2025_09_19_141605_create_orders_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id');
             $table->foreignId('product_id');
-            $table->integer('order n°');
+            $table->integer('order n°')->unique();
             $table->string('statut');
             $table->timestamps();
         });
