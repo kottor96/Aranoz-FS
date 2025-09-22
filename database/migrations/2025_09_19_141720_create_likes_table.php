d3c43d5e540d3d211b14b1bd81e68b2d96ad5e61
kottor <gardien243@gmail.com>
2025-09-22 14:18:04 +0200

    likes

--- database/migrations/2025_09_19_141720_create_likes_table.php
+++ database/migrations/2025_09_19_141720_create_likes_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
+            $table->unique(['user_id','product_id']);
             $table->timestamps();
         });
     }
