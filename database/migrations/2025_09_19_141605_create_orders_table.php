906a36f31a9cdff42cb755d98a9da546c2fbdae2
kottor <gardien243@gmail.com>
2025-09-22 11:18:42 +0200

    ggggg

--- database/migrations/2025_09_19_141605_create_orders_table.php
+++ database/migrations/2025_09_19_141605_create_orders_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('product_id');
+            $table->integer('order n°');
+            $table->string('statut');
             $table->timestamps();
         });
     }
