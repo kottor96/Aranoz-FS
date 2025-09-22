cfea3a2fcb15bc7dffad02a5fc267dc7ded56c8d
kottor <gardien243@gmail.com>
2025-09-22 10:12:34 +0200

    contacts

--- database/migrations/2025_09_19_141528_create_contacts_table.php
+++ database/migrations/2025_09_19_141528_create_contacts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('street');
+            $table->string('state');
+            $table->string('city');
+            $table->foreignId('country_id')->nullable()->constrained('countries')->nullOnDelete();
             $table->timestamps();
         });
     }
