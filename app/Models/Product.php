b3e257482d0dd05602c80b6587cbf1c192f8c238
kottor <gardien243@gmail.com>
2025-09-23 10:24:30 +0200

    je sui aaaa

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,11 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    
+    protected $fillable = ['name','color','description','price','stock','categorie_id','promotion_id'];
     public function specification(){
         return $this->hasOne(Specification::class);
     }
     public function products(){
         return $this->hasMany(Product::class);
     }
+    public function categories(){
+        return $this->belongsToMany(Product_categorie::class,'categorie_id');
+    }
+    public function promotions(){
+        return $this->belongsToMany(Promotion::class);
+    } 
 }
