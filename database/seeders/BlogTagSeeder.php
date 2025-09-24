0abf46b470dab8be77295cc99c684a6858e04622
kottor <gardien243@gmail.com>
2025-09-24 09:10:20 +0200

    mardi

--- database/seeders/BlogTagSeeder.php
+++ database/seeders/BlogTagSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog_tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,22 @@ class BlogTagSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $liste = [
+
+            ['blog_id' => 1, 'tag_id' => 2],
+            ['blog_id' => 1, 'tag_id' => 12],
+
+            ['blog_id' => 2, 'tag_id' => 3],
+
+            ['blog_id' => 3, 'tag_id' => 1],
+
+            ['blog_id' => 4, 'tag_id' => 9],
+
+            ['blog_id' => 5, 'tag_id' => 4],
+            ['blog_id' => 5, 'tag_id' => 11],
+        ];
+        foreach ($liste as $v) {
+            Blog_tag::create($v);
+        }
     }
 }
