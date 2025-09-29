8b2564659053612be0dda041f6d217432c3b2a23
kottor <gardien243@gmail.com>
2025-09-29 15:19:13 +0200

    ok c'est bon

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,6 +12,13 @@ class PageController extends Controller
     public function home(){
         $products = Product::with('images')->get();
         $categories = Product_categorie::with('products','products.images')->get(); 
-        return Inertia::render('Index',compact('products','categories'));
+
+        $maxHours = 300;
+        $maxSeconds = $maxHours * 3600;
+
+        $startTimestamp = strtotime('2025-09-29 00:00:00');
+        $updateInterval = 1000;
+
+        return Inertia::render('Index',compact('products','categories','maxSeconds',"startTimestamp",'updateInterval'));
     }
 }
