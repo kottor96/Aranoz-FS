4c0e654a414cccc5eef9f4a2ea80f56b523d70f4
kottor <gardien243@gmail.com>
2025-09-30 11:25:18 +0200

    passe a blog

---
+++ resources/js/Pages/blog/Blog.jsx
@@ -0,0 +1,11 @@
+import Layout from '@/Layouts/Layout'
+import React from 'react'
+import BlogSection from './Components/blogSection'
+
+export default function Blog() {
+  return (
+    <Layout>
+        <BlogSection />
+    </Layout>
+  )
+}
