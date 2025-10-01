fead38b0f867d69abb4803a96e836df06ddda553
kottor <gardien243@gmail.com>
2025-10-01 09:23:07 +0200

    new days

--- resources/js/Pages/shop/Shop.jsx
+++ resources/js/Pages/shop/Shop.jsx
@@ -1,9 +1,10 @@
 import IntroSection from "@/Components/IntroSection"
 import SectionShop from "./Components/SectionShop"
+import Layout from "@/Layouts/Layout"
 
 export default function Shop({products,categories}){
-    return <>
+    return <Layout>
         <IntroSection titre={"Shop Category"} text={"Home - Shop Category"}/>
         <SectionShop products={products} categories={categories}/>
-    </>
+    </Layout>
 }
